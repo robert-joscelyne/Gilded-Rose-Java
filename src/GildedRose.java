2a2869628d1de4ad1c5267a1cc1a18aeaa6e2f94
Robert Joscelyne <robert.joscelyne@gmail.com>
2018-07-11 17:41:13 +0100

    Refactor Normal items

--- src/GildedRose.java
+++ src/GildedRose.java
@@ -14,32 +14,45 @@ public class GildedRose {
         return new GildedRose(name, quality, sellIn);
     }
 
-    public void tick()
-    {
-        if (this.name != "Aged Brie" && this.name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (this.quality > 0) {
-            if (this.name != "Sulfuras, Hand of Ragnaros") {
-                this.quality = this.quality - 1;
-            }
-        }
-    } else {
-        if (this.quality < 50) {
-            this.quality = this.quality + 1;
+    public void tick() {
+        if (this.name == "normal") {
+            this.sellIn -= 1;
+            this.quality -= 1;
 
-            if (this.name == "Backstage passes to a TAFKAL80ETC concert") {
-                if (this.sellIn < 11) {
-                    if (this.quality < 50) {
-                        this.quality = this.quality + 1;
-                    }
+            if (this.sellIn <= 0) {
+                this.quality -= 1;
+            }
+            if (this.quality < 0) {
+                this.quality = 0;
+            }
+
+            return;
+        }
+
+        if (this.name != "Aged Brie" && this.name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (this.quality > 0) {
+                if (this.name != "Sulfuras, Hand of Ragnaros") {
+                    this.quality = this.quality - 1;
                 }
-                if (this.sellIn < 6) {
-                    if (this.quality < 50) {
-                        this.quality = this.quality + 1;
+            }
+        } else {
+            if (this.quality < 50) {
+                this.quality = this.quality + 1;
+
+                if (this.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (this.sellIn < 11) {
+                        if (this.quality < 50) {
+                            this.quality = this.quality + 1;
+                        }
+                    }
+                    if (this.sellIn < 6) {
+                        if (this.quality < 50) {
+                            this.quality = this.quality + 1;
+                        }
                     }
                 }
             }
         }
-    }
 
         if (this.name != "Sulfuras, Hand of Ragnaros") {
             this.sellIn = this.sellIn - 1;
